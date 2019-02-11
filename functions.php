bf223ceb09ea030624d7caac868e846063545e1c
Yury Lysyuk <lysyuk.y@gmail.com>
2019-02-11 11:12:52 +0300

    initial commit

---
+++ functions.php
@@ -0,0 +1,196 @@
+<?php
+/**
+ * Functions
+ *
+ * @package      Etidni
+ * @author       Yuriy Lysyuk
+ * @since        1.0.0
+ * @license      GPL-2.0+
+**/
+
+/**
+ * Set up the content width value based on the theme's design.
+ *
+ */
+if ( ! isset( $content_width ) )
+    $content_width = 1280;
+
+/**
+ * Theme setup.
+ *
+ * Attach all of the site-wide functions to the correct hooks and filters. All
+ * the functions themselves are defined below this setup function.
+ *
+ * @since 1.0.0
+ */
+function etidni_setup() {
+
+	define( 'CHILD_THEME_VERSION', filemtime( get_stylesheet_directory() . '/assets/css/main.min.css' ) );
+
+	// Includes
+	include_once( get_stylesheet_directory() . '/inc/wordpress-cleanup.php' );
+	include_once( get_stylesheet_directory() . '/inc/genesis-changes.php' );
+	include_once( get_stylesheet_directory() . '/inc/markup.php' );
+	include_once( get_stylesheet_directory() . '/inc/login-logo.php' );
+  include_once( get_stylesheet_directory() . '/inc/tinymce.php' );
+	include_once( get_stylesheet_directory() . '/inc/disable-editor.php' );
+	include_once( get_stylesheet_directory() . '/inc/helper-functions.php' );
+	include_once( get_stylesheet_directory() . '/inc/navigation.php' );
+	include_once( get_stylesheet_directory() . '/inc/loop.php' );
+	// include_once( get_stylesheet_directory() . '/inc/custom-logo.php' );
+
+	// Editor Styles
+	add_theme_support( 'editor-styles' );
+	add_editor_style( 'assets/css/editor-style.css' );
+
+	// Image Sizes
+	// add_image_size( 'Etidni_featured', 400, 100, true );
+
+	// Gutenberg
+
+	// -- Wide Images
+	add_theme_support( 'align-wide' );
+
+	// -- Disable custom font sizes
+	add_theme_support( 'disable-custom-font-sizes' );
+
+	// -- Editor Font Styles
+	add_theme_support( 'editor-font-sizes', array(
+		array(
+			'name'      => __( 'small', 'Etidni' ),
+			'shortName' => __( 'S', 'Etidni' ),
+			'size'      => 12,
+			'slug'      => 'small'
+		),
+		array(
+			'name'      => __( 'regular', 'Etidni' ),
+			'shortName' => __( 'M', 'Etidni' ),
+			'size'      => 16,
+			'slug'      => 'regular'
+		),
+		array(
+			'name'      => __( 'large', 'Etidni' ),
+			'shortName' => __( 'L', 'Etidni' ),
+			'size'      => 20,
+			'slug'      => 'large'
+		),
+	) );
+
+	// -- Disable Custom Colors
+	add_theme_support( 'disable-custom-colors' );
+
+	// -- Editor Color Palette
+	add_theme_support( 'editor-color-palette', array(
+		array(
+			'name'  => __( 'Blue', 'Etidni' ),
+			'slug'  => 'blue',
+			'color'	=> '#59BACC',
+		),
+		array(
+			'name'  => __( 'Green', 'Etidni' ),
+			'slug'  => 'green',
+			'color' => '#58AD69',
+		),
+		array(
+			'name'  => __( 'Orange', 'Etidni' ),
+			'slug'  => 'orange',
+			'color' => '#FFBC49',
+		),
+		array(
+			'name'	=> __( 'Red', 'Etidni' ),
+			'slug'	=> 'red',
+			'color'	=> '#E2574C',
+		),
+	) );
+
+}
+add_action( 'genesis_setup', 'etidni_setup', 15 );
+
+/**
+ * Change the comment area text
+ *
+ * @since  1.0.0
+ * @param  array $args
+ * @return array
+ */
+function etidni_comment_text( $args ) {
+	$args['title_reply']          = __( 'Leave A Reply', 'Etidni' );
+	$args['label_submit']         = __( 'Post Comment',  'Etidni' );
+	$args['comment_notes_before'] = '';
+	$args['comment_notes_after']  = '';
+	return $args;
+}
+add_filter( 'comment_form_defaults', 'etidni_comment_text' );
+
+/**
+ * Global enqueues
+ *
+ * @since  1.0.0
+ * @global array $wp_styles
+ */
+function etidni_global_enqueues() {
+
+	// javascript
+	wp_enqueue_script( 'etidni-script', get_stylesheet_directory_uri() . '/assets/js/scripts.min.js', array( 'jquery' ), filemtime( get_stylesheet_directory() . '/assets/js/scripts.min.js' ), true );
+
+	// css
+  wp_dequeue_style( 'child-theme' );
+  wp_enqueue_style( 'etidni-style', get_stylesheet_directory_uri() . '/assets/css/main.min.css', array(), CHILD_THEME_VERSION );
+
+	// Move jQuery to footer
+	if( ! is_admin() ) {
+		wp_deregister_script( 'jquery' );
+		wp_register_script( 'jquery', get_stylesheet_directory_uri() . '/assets/js/jquery-3.3.1.min.js', false, NULL, true );
+		wp_enqueue_script( 'jquery' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'etidni_global_enqueues' );
+
+/**
+ * Включаем асинхронную загрузку некоторых скриптов
+ *
+ */
+function etidni_add_async_attribute($tag, $handle) {
+   // добавьте дескрипторы (названия) скриптов в массив ниже
+   $scripts_to_async = array('etidni-script');
+   
+   foreach($scripts_to_async as $async_script) {
+      if ($async_script === $handle) {
+         return str_replace(' src', ' async src', $tag);
+      }
+   }
+   return $tag;
+}
+
+add_filter('script_loader_tag', 'etidni_add_async_attribute', 10, 2);
+
+/**
+ * Gutenberg scripts and styles
+ *
+ */
+function etidni_gutenberg_scripts() {
+
+}
+add_action( 'enqueue_block_editor_assets', 'etidni_gutenberg_scripts' );
+
+
+/**
+ * Template Hierarchy
+ *
+ */
+function etidni_template_hierarchy( $template ) {
+	if( is_home() || is_search() )
+		$template = get_query_template( 'archive' );
+	return $template;
+}
+add_filter( 'template_include', 'etidni_template_hierarchy' );
+
+/**
+ * Add favicon
+ *
+ */
+function ly_favicon() {
+	echo '<link rel="icon" href="' . get_stylesheet_directory_uri() . '/assets/images/favicon-152.png">';
+	echo '<!--[if IE]><link rel="shortcut icon" href="' . get_stylesheet_directory_uri() . '/assets/images/favicon.ico"><![endif]-->';	
+}
+add_action( 'wp_head', 'ly_favicon' );
---
+++ inc/wordpress-cleanup.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * WordPress Cleanup
+ *
+ * @package      Etidni
+ * @author       Yuriy Lysyuk
+ * @since        1.0.0
+ * @license      GPL-2.0+
+**/
+
+ /**
+  * Dont Update the Theme
+  *
+  * If there is a theme in the repo with the same name, this prevents WP from prompting an update.
+  *
+  * @since  1.0.0
+  * @author Bill Erickson
+  * @link   http://www.billerickson.net/excluding-theme-from-updates
+  * @param  array $r Existing request arguments
+  * @param  string $url Request URL
+  * @return array Amended request arguments
+  */
+ function ea_dont_update_theme( $r, $url ) {
+ 	if ( 0 !== strpos( $url, 'https://api.wordpress.org/themes/update-check/1.1/' ) )
+  		return $r; // Not a theme update request. Bail immediately.
+  	$themes = json_decode( $r['body']['themes'] );
+  	$child = get_option( 'stylesheet' );
+ 	unset( $themes->themes->$child );
+  	$r['body']['themes'] = json_encode( $themes );
+  	return $r;
+  }
+ add_filter( 'http_request_args', 'ea_dont_update_theme', 5, 2 );
+
+/**
+ * Clean Nav Menu Classes
+ *
+ */
+function ea_clean_nav_menu_classes( $classes ) {
+	if( ! is_array( $classes ) )
+		return $classes;
+
+	foreach( $classes as $i => $class ) {
+
+		// Remove class with menu item id
+		$id = strtok( $class, 'menu-item-' );
+		if( 0 < intval( $id ) )
+			unset( $classes[ $i ] );
+
+		// Remove menu-item-type-*
+		if( false !== strpos( $class, 'menu-item-type-' ) )
+			unset( $classes[ $i ] );
+
+		// Remove menu-item-object-*
+		if( false !== strpos( $class, 'menu-item-object-' ) )
+			unset( $classes[ $i ] );
+
+		// Change page ancestor to menu ancestor
+		if( 'current-page-ancestor' == $class ) {
+			$classes[] = 'current-menu-ancestor';
+			unset( $classes[ $i ] );
+		}
+	}
+
+	// Remove submenu class if depth is limited
+	if( isset( $args->depth ) && 1 === $args->depth ) {
+		$classes = array_diff( $classes, array( 'menu-item-has-children' ) );
+	}
+
+	return $classes;
+}
+add_filter( 'nav_menu_css_class', 'ea_clean_nav_menu_classes', 5 );
+
+/**
+ * Clean Post Classes
+ *
+ */
+function ea_clean_post_classes( $classes ) {
+
+	if( ! is_array( $classes ) )
+		return $classes;
+
+    $allowed_classes = array(
+  		'hentry',
+  		'type-' . get_post_type(),
+      'one-half',
+      'one-third',
+      'two-thirds',
+      'one-fourth',
+      'two-fourths',
+      'three-fourths',
+      'one-fifth',
+      'two-fifths',
+      'three-fifths',
+      'four-fifths',
+   	);
+
+	return array_intersect( $classes, $allowed_classes );
+}
+add_filter( 'post_class', 'ea_clean_post_classes', 5 );
+
+/**
+ * Staff comment class
+ *
+ */
+function ea_staff_comment_class( $classes, $class, $comment_id, $comment, $post_id ) {
+	if( empty( $comment->user_id ) )
+		return $classes;
+	$staff_roles = array( 'comment_manager', 'author', 'editor', 'administrator' );
+	$staff_roles = apply_filters( 'ea_staff_roles', $staff_roles );
+	$user = get_userdata( $comment->user_id );
+	if( !empty( array_intersect( $user->roles, $staff_roles ) ) )
+		$classes[] = 'staff';
+	return $classes;
+}
+add_filter( 'comment_class', 'ea_staff_comment_class', 10, 5 );
+
+/**
+ * Remove avatars from comment list
+ *
+ */
+function ea_remove_avatars_from_comments( $avatar ) {
+	global $in_comment_loop;
+	return $in_comment_loop ? '' : $avatar;
+}
+add_filter( 'get_avatar', 'ea_remove_avatars_from_comments' );
+
+/**
+ * Excerpt More
+ *
+ */
+function ea_excerpt_more() {
+	return '&hellip;';
+}
+add_filter( 'excerpt_more', 'ea_excerpt_more' );
