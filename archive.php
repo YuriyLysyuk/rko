aa8a231e531ff746339e6cf4ff73bd9e63d2593a
Yury Lysyuk <lysyuk.y@gmail.com>
2019-03-11 16:45:47 +0300

    На страницах пагинации убрал дублирующийся текст

--- archive.php
+++ archive.php
@@ -21,6 +21,12 @@ function ea_blog_archive_body_class( $classes ) {
 }
 add_filter( 'body_class', 'ea_blog_archive_body_class' );
 
+// На страницах пагинации в архивах убираем заголовок и описание
+if (is_paged()) {
+	remove_action( 'genesis_archive_title_descriptions', 'genesis_do_archive_headings_headline', 10, 3 );
+	remove_action ( 'genesis_archive_title_descriptions', 'genesis_do_archive_headings_intro_text', 12, 3 );
+}
+
 if (is_search()) {
 	remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
 
