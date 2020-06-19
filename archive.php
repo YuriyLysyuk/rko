b73ff09edbede80df1604a416a6df2cea684bab3
Yury Lysyuk <lysyuk.y@gmail.com>
2020-06-19 14:09:18 +0300

    добавил вывод заголовка для блога

--- archive.php
+++ archive.php
@@ -4,25 +4,36 @@
  *
  * @package      rko
  * @author       Yuriy Lysyuk
- * @since        1.0.3
-**/
+ * @since        1.3.0
+ **/
 
 /**
  * Blog Archive Body Class
  *
  */
-function ea_blog_archive_body_class( $classes ) {
-	if (!is_search()){
-		$classes[] = 'archive';
-	}
-	return $classes;
+function ea_blog_archive_body_class($classes)
+{
+  if (!is_search()) {
+    $classes[] = 'archive';
+  }
+  return $classes;
 }
-add_filter( 'body_class', 'ea_blog_archive_body_class' );
+add_filter('body_class', 'ea_blog_archive_body_class');
 
 // На страницах пагинации в архивах убираем заголовок и описание
 if (is_paged()) {
-	remove_action( 'genesis_archive_title_descriptions', 'genesis_do_archive_headings_headline', 10, 3 );
-	remove_action ( 'genesis_archive_title_descriptions', 'genesis_do_archive_headings_intro_text', 12, 3 );
+  remove_action(
+    'genesis_archive_title_descriptions',
+    'genesis_do_archive_headings_headline',
+    10,
+    3
+  );
+  remove_action(
+    'genesis_archive_title_descriptions',
+    'genesis_do_archive_headings_intro_text',
+    12,
+    3
+  );
 }
 
 genesis();
