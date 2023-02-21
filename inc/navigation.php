e274a4b19b2f92b5d432b6a6fd79a92d27771abd
YuriyLysyuk <work@yuriylysyuk.ru>
2023-02-21 23:12:02 +0300

    обновление стиля кода

--- inc/navigation.php
+++ inc/navigation.php
@@ -8,28 +8,27 @@
  **/
 
 // Primary Nav in Header
-remove_action('genesis_after_header', 'genesis_do_nav');
-add_action('genesis_header', 'genesis_do_nav', 12);
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+add_action( 'genesis_header', 'genesis_do_nav', 12 );
 
 /**
  * Мобильное меню с формой поиска
  *
  */
-function rko_mobile_menu()
-{
-  echo '<nav class="nav-mobile">';
-  // echo '<button class="search-toggle">';
-  // echo ea_icon(array('icon' => 'search', 'size' => 16, 'class' => 'open'));
-  // echo ea_icon(array('icon' => 'close', 'size' => 16, 'class' => 'close'));
-  // echo '</button>';
+function rko_mobile_menu() {
+	echo '<nav class="nav-mobile">';
+	// echo '<button class="search-toggle">';
+	// echo ea_icon(array('icon' => 'search', 'size' => 16, 'class' => 'open'));
+	// echo ea_icon(array('icon' => 'close', 'size' => 16, 'class' => 'close'));
+	// echo '</button>';
 
-  echo '<button class="mobile-menu-toggle">';
-  echo ea_icon(array('icon' => 'menu', 'size' => 16, 'class' => 'open'));
-  echo ea_icon(array('icon' => 'close', 'size' => 16, 'class' => 'close'));
-  echo '</button>';
-  echo '</nav>';
+	echo '<button class="mobile-menu-toggle">';
+	echo ea_icon( array( 'icon' => 'menu', 'size' => 16, 'class' => 'open' ) );
+	echo ea_icon( array( 'icon' => 'close', 'size' => 16, 'class' => 'close' ) );
+	echo '</button>';
+	echo '</nav>';
 }
-add_action('genesis_header', 'rko_mobile_menu', 11);
+add_action( 'genesis_header', 'rko_mobile_menu', 11 );
 
 /**
  * Add a dropdown icon to top-level menu items.
@@ -41,22 +40,21 @@ add_action('genesis_header', 'rko_mobile_menu', 11);
  * @return string Nav menu item start element.
  * Add a dropdown icon to top-level menu items
  */
-function ea_nav_add_dropdown_icons($output, $item, $depth, $args)
-{
-  if (!isset($args->theme_location) || 'primary' !== $args->theme_location) {
-    return $output;
-  }
+function ea_nav_add_dropdown_icons( $output, $item, $depth, $args ) {
+	if ( ! isset( $args->theme_location ) || 'primary' !== $args->theme_location ) {
+		return $output;
+	}
 
-  if (in_array('menu-item-has-children', $item->classes, true)) {
-    // Add SVG icon to parent items.
-    $icon = ea_icon(array('icon' => 'navigate-down', 'size' => 16));
+	if ( in_array( 'menu-item-has-children', $item->classes, true ) ) {
+		// Add SVG icon to parent items.
+		$icon = ea_icon( array( 'icon' => 'navigate-down', 'size' => 16 ) );
 
-    $output .= sprintf(
-      '<button class="submenu-expand" tabindex="-1">%s</button>',
-      $icon
-    );
-  }
+		$output .= sprintf(
+			'<button class="submenu-expand" tabindex="-1">%s</button>',
+			$icon
+		);
+	}
 
-  return $output;
+	return $output;
 }
-add_filter('walker_nav_menu_start_el', 'ea_nav_add_dropdown_icons', 10, 4);
+add_filter( 'walker_nav_menu_start_el', 'ea_nav_add_dropdown_icons', 10, 4 );
--- inc/search.php
+++ inc/search.php
@@ -12,28 +12,26 @@
  *
  */
 
-function ea_search_form()
-{
-  ob_start();
-  get_template_part('searchform');
-  return ob_get_clean();
+function ea_search_form() {
+	ob_start();
+	get_template_part( 'searchform' );
+	return ob_get_clean();
 }
-add_filter('genesis_search_form', 'ea_search_form');
+add_filter( 'genesis_search_form', 'ea_search_form' );
 
 /**
  * Добавляем кнопку поиска в главное меню
  *
  */
-function ea_primary_menu_extras($menu, $args)
-{
-  if ('primary' == $args->theme_location) {
-    $menu .=
-      '<li class="menu-item search"><button href="#" class="search-toggle">' .
-      ea_icon(array('icon' => 'search', 'size' => 16, 'class' => 'open')) .
-      ea_icon(array('icon' => 'close', 'size' => 16, 'class' => 'close')) .
-      '</button></li>';
-  }
-  return $menu;
+function ea_primary_menu_extras( $menu, $args ) {
+	if ( 'primary' == $args->theme_location ) {
+		$menu .=
+			'<li class="menu-item search"><button href="#" class="search-toggle">' .
+			ea_icon( array( 'icon' => 'search', 'size' => 16, 'class' => 'open' ) ) .
+			ea_icon( array( 'icon' => 'close', 'size' => 16, 'class' => 'close' ) ) .
+			'</button></li>';
+	}
+	return $menu;
 }
 // add_filter('wp_nav_menu_items', 'ea_primary_menu_extras', 10, 2);
 
@@ -41,8 +39,7 @@ function ea_primary_menu_extras($menu, $args)
  * Добавляем форму поиска
  *
  */
-function ly_header_search()
-{
-  get_template_part('partials/headersearch');
+function ly_header_search() {
+	get_template_part( 'partials/headersearch' );
 }
-add_action('genesis_after_header', 'ly_header_search', 12);
+add_action( 'genesis_after_header', 'ly_header_search', 12 );
